f1b174061d8ea52903e6425d3c89e0f80825ff57
Siyuan Chen <daniel@apextone.com>
2020-05-16 23:15:09 -0400

    Removed duplicate method in Body; added diagram for presentation

--- src/main/java/map/Body.java
+++ src/main/java/map/Body.java
@@ -6,46 +6,11 @@ import java.util.ArrayList;
 @ClassPreamble (
         author = "Daniel Chen",
         date = "01/14/2020",
-        currentRevision = 5.2,
-        lastModified = "05/09/2020",
+        currentRevision = 5.3,
+        lastModified = "05/16/2020",
         lastModifiedBy = "Daniel Chen"
 )
 public abstract class Body {
-    
-    /**
-     * returns the four corners of the rectangle described by the parameters
-     * 
-     * @param size the Size of the rectangle
-     * @param position the Position of the center of the rectangle
-     * @param velocity defines the orientation of the rectangle
-     * @return the four Position objects of the rectangle described
-     */
-    public static ArrayList<Position> getCornerPositions(Size size, Position position, Velocity velocity) {
-        
-        ArrayList<Position> cornerPositions = new ArrayList<Position>();
-        
-        double halfDiagonal = Math.sqrt(Math.pow(size.getAlong(), 2) + Math.pow(size.getAcross(), 2)) / 2;
-        double angle1 = velocity.getOrientation() + Math.atan2(size.getAcross(), size.getAlong());
-        double angle2 = velocity.getOrientation() - Math.atan2(size.getAcross(), size.getAlong());
-        
-        cornerPositions.add(new Position(
-                position.getXPosition() + halfDiagonal * Math.cos(angle1),
-                position.getYPosition() + halfDiagonal * Math.sin(angle1)));
-        
-        cornerPositions.add(new Position(
-                position.getXPosition() - halfDiagonal * Math.cos(angle1),
-                position.getYPosition() - halfDiagonal * Math.sin(angle1)));
-        
-        cornerPositions.add(new Position(
-                position.getXPosition() + halfDiagonal * Math.cos(angle2),
-                position.getYPosition() + halfDiagonal * Math.sin(angle2)));
-        
-        cornerPositions.add(new Position(
-                position.getXPosition() - halfDiagonal * Math.cos(angle2),
-                position.getYPosition() - halfDiagonal * Math.sin(angle2)));
-        
-        return cornerPositions;
-    }
     
     private Size size;
     private Position position;
@@ -81,7 +46,7 @@ public abstract class Body {
      */
     public ArrayList<Position> getCornerPositions() {
         
-        ArrayList<Position> cornerPositions = new ArrayList<Position>();
+        ArrayList<Position> cornerPositions = new ArrayList<>();
         
         double halfDiagonal = Math.sqrt(Math.pow(size.getAlong(), 2) + Math.pow(size.getAcross(), 2)) / 2;
         double angle1 = velocity.getOrientation() + Math.atan2(size.getAcross(), size.getAlong());
